156f6968eb1cc6ea94268fffaebad9ed8b12eb33
Solomon Nsumei <solnsumei@gmail.com>
2023-08-19 01:38:47 +0100

    scaffold api project to serve the blazor app

---
+++ BlazingTrails.Api/Persistence/Entities/Trail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BlazingTrails.Client.Features.Home;
+
+namespace BlazingTrails.Api.Persistence.Entities;
+
+public class Trail
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = default!;
+    [Required]
+    public string Description { get; set; } = default!;
+    public string? Image { get; set; }
+    [Required]
+    public string Location { get; set; } = default!;
+    [Required]
+    public int TimeInMinutes { get; set; }
+    [Required]
+    public int Length { get; set; }
+
+    public ICollection<RouteInstruction> Route { get; set; } = default!;
+}
+
